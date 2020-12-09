ebd9a747e58b7c683537af17ab38c84f7bc5fd29
Javier Gonzalez Benito <javi.gb.mad@gmail.com>
2020-12-09 16:40:43 +0100

    Trust Digital - Hachathon 2020

---
+++ src/main/java/com/santander/renting/houserenting/HouseRentingApplication.java
@@ -0,0 +1,13 @@
+package com.santander.renting.houserenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseRentingApplication.class, args);
+	}
+
+}
